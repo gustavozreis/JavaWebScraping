feeeaf2f3ef97baa74cf3ceffd648627f7b846af
gustavozreis <gzreis@gmail.com>
2022-03-16 18:19:37 -0300

    Adicionado Jsoup lib.
    Biblioteca JSoup adicionada e testada, consegui recuperar o HTML inteiro do site.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,25 @@
 package application;
 
-public class Program {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		
+	final String url = "https://www.pensador.com/frases_de_motivacao/1/";
+	
+		try {
+			Document doc = Jsoup.connect(url).get();
+			
+			String fullHtml = doc.outerHtml();
+			
+			System.out.println(fullHtml);
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	
+	}
 }
