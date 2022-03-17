d2f9521a0207f7e697db8d6d3d05df130093acf4
gustavozreis <gzreis@gmail.com>
2022-03-17 17:46:27 -0300

    Configurado lógica para colocar aspas no início e final das frases.
    Primeiro passo em devolver um documento de lista pronto para ser usado no Android. As frases agora são adicionadas na lista de frases já com as aspas necessárias para se declarar uma string.

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,8 @@ public class Program {
 	// instancia da lista de frases a ser preenchida
 	ArrayList<String> listaDeFrases = new ArrayList<String>();
 	
-	for (Integer i=1; i<500; i++ ) {
+	
+	for (Integer i=1; i<10; i++ ) {
 	
 		try {
 			// conecta o programa ao website
@@ -25,24 +26,30 @@ public class Program {
 			// encontra as frases pelo id da classe no html
 			Elements frases = doc.getElementsByClass("frase fr");
 			// adiciona a frase na lista de frases
-			for (Element frase : frases) {
+			for (Element frase : frases) {		
+				
 				if(!frase.text().toUpperCase().contains("JESUS") 
 						&& !frase.text().toUpperCase().contains("DEUS")
 						&& !frase.text().toUpperCase().contains("IGREJA")
 						&& !frase.text().toUpperCase().contains("CRISTO")
-						&& frase.text().length() < 65)
-				listaDeFrases.add(frase.text());
+						&& frase.text().length() < 65) {
+						
+					 	String aspas = "\"";
+					 	String fraseComAspas = aspas + frase.text() + aspas;
+						listaDeFrases.add(fraseComAspas);	
+						}
+			}		
+			
 			}
-			
+			catch (Exception ex) {
+				ex.printStackTrace();
+			}	
+				
+			}
+	
+		for (int i=0; i<listaDeFrases.size(); i++) {
+			System.out.println(listaDeFrases.get(i));
 		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}	
-			
-		}
-	for (int i=0; i<listaDeFrases.size(); i++) {
-		System.out.println(listaDeFrases.get(i));
-	}
-	System.out.println("Quantidade de Frases Recuperadas: " + listaDeFrases.size());
+		System.out.println("Quantidade de Frases Recuperadas: " + listaDeFrases.size());
 	}
 }
