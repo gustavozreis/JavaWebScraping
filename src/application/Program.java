526e30d92284ede218a772ff02470cd5cd6e24e1
gustavozreis <gzreis@gmail.com>
2022-03-17 14:40:48 -0300

    Divisão da string de frases e adicioná-las uma por uma em uma arraylist.
    Configurei a divisão da string com todas as frases em frases individuais e cada uma delas é inserida em uma arraylist de frases. Agora preciso fazer a lógica para que o programa repita essa operação em todas as 2000 páginas do website.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,18 +14,22 @@ public class Program {
 		
 	final String url = "https://www.pensador.com/frases_de_motivacao/1/";
 	
+	ArrayList<String> listaDeFrases = new ArrayList<String>();
+	
 		try {
 			Document doc = Jsoup.connect(url).get();
 			
 			Elements frases = doc.getElementsByClass("frase fr");
 			
 			for (Element frase : frases) {
-				System.out.println(frase.text());
+				listaDeFrases.add(frase.text());
 			}
 		}
 		catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		
+		System.out.println(listaDeFrases);
 	
 	}
 }
